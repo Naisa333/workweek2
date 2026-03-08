e4f6159b6f3cb936bba2f3d558691b48295bc3af
Naisa <bnaisa@student.refactory.academy>
2026-03-08 10:54:40 -0700

    initial

---
+++ dataTypes.js
@@ -0,0 +1,41 @@
+let hoursParked = '4'
+let hourlyRate = 2000;
+let hours = Number(hoursParked);//constructor function
+let totalfee = hours * hourlyRate
+console.log(totalfee);
+
+function car(){
+
+}
+function Car(model,year,make,arrivaTime,isloggedIn,){
+       this. make=make;
+       this. model=model;
+        this.year=year;newDate()
+        this.isloggedIn = false;
+        arrivaTime = new
+        drive=function(){
+                console.log(`you drive a this. ${this.model}`)
+        }
+}
+ //constructor function, it is a template for creating objects.
+let hamsCar = new Car('corolla', 2020, 'toyota')
+console.log(hamsCar)
+
+let babrasCar = new Car('camry', 2022, 'toyota')
+console.log(babrasCar)
+
+Number()
+String()
+Boolean()
+function Number(number, string, boolean){
+        this.number = number;
+        this.string = string;
+        this.boolean = boolean;
+    }
+    let Number1 = new Number(5, 'hello', true)      
+    console.log(Number1)
+
+    Number(true)
+    
+
+
---
+++ operatorsAndExpressions.js
@@ -0,0 +1,33 @@
+//first category of operators and expressions.
+// an opertor is a symbol that tells js to perform an action
+let  total = 3000 + 2000; 
+//the first category of operatos is called the arithmetic/mathematical operators.
+//operators, +,-,/,%,*
+//assume your told to calculate total 
+let hoursParked = 4
+let hourlyRate = 2000
+let totalFee = hoursParked * hourlyRate
+
+a=b
+a=a+b,  total + =b
+total=b
+total = total+b
+total= total-b
+//the equal sign  is called assiging, = the assignment operator
+//comparision operators.
+//== loosely equal operators
+//===strictly equal operators
+console.log(5=='5')//true
+console.log('5')//false
+//<,> <=, >=, 1= these ar good for decision making, arithmetic for calculating.
+
+console.log(hoursParked<5)
+//logic operators
+//&& and, || or !not 
+//increament and decrement operators.
+// ++ and --
+let count = 0
+console.log(count++)
+console.log(count)
+operator precedence 
+
